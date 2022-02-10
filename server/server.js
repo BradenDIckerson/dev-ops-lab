c0c50a09ad99e05bf98d5026a4495c9bad3e4e14
Braden <purpledragon6445@gmail.com>
2022-02-10 13:28:22 -0700

    second coomit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path');
+const cors = require('cors')
 
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
@@ -9,3 +10,15 @@ const rollbar = new Rollbar({
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
+
+app.use(express.json());
+
+
+
+
+
+
+
+const port = process.env.PORT || process.env.SERVER_PORT
+
+app.listen(port, () => console.log(`Server serving on ${port}`)) 
